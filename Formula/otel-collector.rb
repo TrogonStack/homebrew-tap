4502a4d7a257bda45c967040392926f6e6255ac9
yordis <4237280+yordis@users.noreply.github.com>
2026-04-29 02:34:49 +0000

    feat: bump otel-collector to 0.151.0
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/otel-collector.rb
+++ Formula/otel-collector.rb
@@ -1,9 +1,9 @@
 class OtelCollector < Formula
   desc "OpenTelemetry Collector Contrib - vendor-agnostic implementation"
   homepage "https://github.com/open-telemetry/opentelemetry-collector-contrib"
-  url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.147.0/otelcol-contrib_0.147.0_darwin_amd64.tar.gz"
-  sha256 "f0075b96fcc9d1c085ae76d7206cf2c52080a3237eee4ce36218cafb2c6cd9d5"
-  version "0.147.0"
+  url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.151.0/otelcol-contrib_0.151.0_darwin_amd64.tar.gz"
+  sha256 "2c080d5c92e2742de6f0be1097f3c46446e07f666d892b7708764d28958e8d86"
+  version "0.151.0"
   license "Apache-2.0"
 
   def install
