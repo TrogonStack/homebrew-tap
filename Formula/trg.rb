271ea27fc16ac22c73fa17d815ada7b71589893c
yordis <4237280+yordis@users.noreply.github.com>
2026-05-24 02:51:06 +0000

    feat: bump trg to 0.3.0
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/trg.rb
+++ Formula/trg.rb
@@ -2,29 +2,29 @@ class Trg < Formula
   desc "CLI tool for scaffolding and managing TrogonStack projects"
   homepage "https://github.com/TrogonStack/rusty-monorepo"
   license "MIT"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
     on_arm64 do
-      url "https://github.com/TrogonStack/rusty-monorepo/releases/download/trg@v0.2.0/trg-0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "c25e09673b41d5072ac82dc1720ca714c553501545780862d6fd05799d0f54b1"
+      url "https://github.com/TrogonStack/rusty-monorepo/releases/download/trg@v0.3.0/trg-0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "c5268714d30b851cc1a7f8bb6d7a212440f6e6d5894ce3cd9edb78a92850495d"
     end
 
     on_intel do
-      url "https://github.com/TrogonStack/rusty-monorepo/releases/download/trg@v0.2.0/trg-0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "bbfc52c4e4955376d202b693c6cdd3cdd054bcfb69695304219c017aaaed149f"
+      url "https://github.com/TrogonStack/rusty-monorepo/releases/download/trg@v0.3.0/trg-0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "fbb825318b508859f2fb78f154b8f7baca3e81b2dc34b9403a719072d2d41f05"
     end
   end
 
   on_linux do
     on_arm64 do
-      url "https://github.com/TrogonStack/rusty-monorepo/releases/download/trg@v0.2.0/trg-0.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "7e066ed27098ebf9df869b94a5e6aea190ca65531dd95e4d8d903aa9540c3326"
+      url "https://github.com/TrogonStack/rusty-monorepo/releases/download/trg@v0.3.0/trg-0.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "b9a53869cc38c6a05b6e413cd0b180333713d71c2d0a186e66319e132170a4c7"
     end
 
     on_x86_64 do
-      url "https://github.com/TrogonStack/rusty-monorepo/releases/download/trg@v0.2.0/trg-0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b14c575d609aef875a786ad628f4547a0b22427a1255ae67e32668f0032ff273"
+      url "https://github.com/TrogonStack/rusty-monorepo/releases/download/trg@v0.3.0/trg-0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "fa5cdcfdcf837d99729750bfb2fcf2b4b0858d8fcde793e8e06ae60fe6b2a11c"
     end
   end
 
