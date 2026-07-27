36327410bede7fbae162a01b666ccc5fdf188dd9
yordis <4237280+yordis@users.noreply.github.com>
2026-07-27 09:41:26 +0000

    feat: bump protoc-gen-elixir-grpc to 0.4.3
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/protoc-gen-elixir-grpc.rb
+++ Formula/protoc-gen-elixir-grpc.rb
@@ -2,29 +2,29 @@ class ProtocGenElixirGrpc < Formula
   desc "Protocol Buffers plugin for gRPC with Elixir support"
   homepage "https://github.com/TrogonStack/protoc-gen"
   license "MIT"
-  version "0.4.2"
+  version "0.4.3"
 
   on_macos do
     on_arm64 do
-      url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.2/protoc-gen-elixir-grpc@v0.4.2_Darwin_arm64.tar.gz"
-      sha256 "81d909ace0f6a721e15a82df45b46d97cdf744c82093060cb546b010aad012f1"
+      url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.3/protoc-gen-elixir-grpc@v0.4.3_Darwin_arm64.tar.gz"
+      sha256 "bcc1a4d8212f59e00bdcb58a12080e208a2f18ce70b4270c3b9c626f05e87d37"
     end
 
     on_intel do
-      url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.2/protoc-gen-elixir-grpc@v0.4.2_Darwin_x86_64.tar.gz"
-      sha256 "244c3cb2fea67f8a81c3d550e3dc69c585adeb5d7a9b251510dddf87756c847e"
+      url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.3/protoc-gen-elixir-grpc@v0.4.3_Darwin_x86_64.tar.gz"
+      sha256 "15888f3527a0388497490e4123b6e2e5266d762874d99219b18e21ca54c8aae6"
     end
   end
 
   on_linux do
     on_arm64 do
-      url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.2/protoc-gen-elixir-grpc@v0.4.2_Linux_arm64.tar.gz"
-      sha256 "f20e363ba19f07d33f9a2d511701178abb4548507498d7a8f32537da1833128e"
+      url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.3/protoc-gen-elixir-grpc@v0.4.3_Linux_arm64.tar.gz"
+      sha256 "e10ba9dd24aa0f60b58662897faf1183e47f8bcca12a22cea00695ddcebb6ab4"
     end
 
     on_x86_64 do
-      url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.2/protoc-gen-elixir-grpc@v0.4.2_Linux_x86_64.tar.gz"
-      sha256 "d0264903f5caf958313d3f27f897975604cd6935f968eeda9bba4a9ad2fa3be4"
+      url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.3/protoc-gen-elixir-grpc@v0.4.3_Linux_x86_64.tar.gz"
+      sha256 "a0d0d159b0ab927c19033a9d37785c698d2a3f972da3d73e5eb50c179ccb973b"
     end
   end
 
