292cc2b62c822aa58d5fa0005623c7608b4591a9
Yordis Prieto <yordis.prieto@gmail.com>
2026-07-28 21:55:34 -0400

    fix: use valid Homebrew arch DSL methods in protoc-gen-elixir-grpc
    on_arm64/on_x86_64 aren't real Homebrew methods (only on_arm/on_intel
    exist), so brew audit and Linux installs crashed with NoMethodError.
    
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- Formula/protoc-gen-elixir-grpc.rb
+++ Formula/protoc-gen-elixir-grpc.rb
@@ -5,7 +5,7 @@ class ProtocGenElixirGrpc < Formula
   version "0.4.3"
 
   on_macos do
-    on_arm64 do
+    on_arm do
       url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.3/protoc-gen-elixir-grpc@v0.4.3_Darwin_arm64.tar.gz"
       sha256 "bcc1a4d8212f59e00bdcb58a12080e208a2f18ce70b4270c3b9c626f05e87d37"
     end
@@ -17,12 +17,12 @@ class ProtocGenElixirGrpc < Formula
   end
 
   on_linux do
-    on_arm64 do
+    on_arm do
       url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.3/protoc-gen-elixir-grpc@v0.4.3_Linux_arm64.tar.gz"
       sha256 "e10ba9dd24aa0f60b58662897faf1183e47f8bcca12a22cea00695ddcebb6ab4"
     end
 
-    on_x86_64 do
+    on_intel do
       url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-elixir-grpc@v0.4.3/protoc-gen-elixir-grpc@v0.4.3_Linux_x86_64.tar.gz"
       sha256 "a0d0d159b0ab927c19033a9d37785c698d2a3f972da3d73e5eb50c179ccb973b"
     end
