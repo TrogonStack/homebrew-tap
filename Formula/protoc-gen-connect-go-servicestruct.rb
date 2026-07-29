9b9743d0047275c79e8d580d28499490929bc51c
Yordis Prieto <yordis.prieto@gmail.com>
2026-07-28 21:55:18 -0400

    fix: use valid Homebrew arch DSL methods in formulas
    on_arm64/on_x86_64 aren't real Homebrew methods (only on_arm/on_intel
    exist), so brew audit and Linux installs crashed with NoMethodError.
    
    Signed-off-by: Yordis Prieto <yordis.prieto@gmail.com>

--- Formula/protoc-gen-connect-go-servicestruct.rb
+++ Formula/protoc-gen-connect-go-servicestruct.rb
@@ -5,7 +5,7 @@ class ProtocGenConnectGoServicestruct < Formula
   version "0.2.0"
 
   on_macos do
-    on_arm64 do
+    on_arm do
       url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-connect-go-servicestruct@v0.2.0/protoc-gen-connect-go-servicestruct@v0.2.0_Darwin_arm64.tar.gz"
       sha256 "8de46260aec64f51f23bef54a7202a3cae76d11ce4c71123b9d01a51cb3cec3d"
     end
@@ -17,12 +17,12 @@ class ProtocGenConnectGoServicestruct < Formula
   end
 
   on_linux do
-    on_arm64 do
+    on_arm do
       url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-connect-go-servicestruct@v0.2.0/protoc-gen-connect-go-servicestruct@v0.2.0_Linux_arm64.tar.gz"
       sha256 "f99390273919fe85c11bee13fda7ee2c5f052ffc96d176edee9349a52c34d5d7"
     end
 
-    on_x86_64 do
+    on_intel do
       url "https://github.com/TrogonStack/protoc-gen/releases/download/protoc-gen-connect-go-servicestruct@v0.2.0/protoc-gen-connect-go-servicestruct@v0.2.0_Linux_x86_64.tar.gz"
       sha256 "ceb785e587bcdb4d913d576cba870f729fe64fc6acc5a7cf30bf617dcc91cfc8"
     end
